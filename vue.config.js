b65cb3478c8f3887404c1ffabebb573ef0bef426
ConnorH14 <ConnorH14@live.com>
2021-06-14 01:36:08 -0600

    config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
-  publicPath: '/deploy-social/',
+  publicPath: '/social-checkpoint/',
   outputDir: 'docs',
   productionSourceMap: false
 }
